427906e7dc0325923efa307ad6a10e07861d9a6a
MarcHedeby <marc@hedeby.net>
2024-05-16 11:20:58 +0200

    added get for spyingobjects

--- BygSpy/Models/SpyModel.cs
+++ BygSpy/Models/SpyModel.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BygSpy.Models
 {
@@ -8,16 +9,40 @@ namespace BygSpy.Models
         //public string Address { get; set; }
         //public int status { get; set; }
         //public DateTime VirkningFra { get; set; }
-        [BsonElement("Id")]
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
         public string Id { get; set; }
+
+        [BsonElement("BFE")]
+        public int BFE { get; set; }
+
+        [BsonElement("Street")]
+        public string Street { get; set; }
+        [BsonElement("Status")]
+        public int Status { get; set; }
+        [BsonElement("City")]
+        public string City { get; set; }
+
+        [BsonElement("SpyName")]
+        public string SpyName { get; set; }
 
         [BsonElement("newobjectName")]
         public string newobjectName { get; set; }
 
+
+        //[BsonElement("Id")]
+        //public string Id { get; set; }
+
+        //[BsonElement("newobjectName")]
+        //public string newobjectName { get; set; }
+
         [BsonElement("adress")]
         public string adress { get; set; }
 
-        [BsonElement("SpyName")]
-        public string SpyName { get; set; }
+        //[BsonElement("SpyName")]
+        //public string SpyName { get; set; }
+
+
+
     }
 }
